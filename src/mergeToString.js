9ca9fdbcafdc83fbf36b134b2731f557fb293e28
Hovhannes Babayan <bhovhannes@gmail.com>
2020-11-25 19:22:17 +0400

    refactor: make code shorter

--- src/mergeToString.js
+++ src/mergeToString.js
@@ -7,47 +7,35 @@ const { DOMParser, XMLSerializer } = require("xmldom");
  * @return {String}
  */
 function mergeToString(srcStrings, options) {
-    let combinedTestsDoc = new DOMParser().parseFromString(
+    const {
+        documentElement: combinedTestSuitesNode,
+    } = new DOMParser().parseFromString(
         '<?xml version="1.0"?>\n<testsuites></testsuites>',
         "text/xml"
     );
-    let combinedTestSuitesNode = combinedTestsDoc.documentElement;
 
-    let failures = 0,
-        errors = 0,
-        tests = 0;
+    const attrs = {
+        failures: 0,
+        errors: 0,
+        tests: 0,
+    };
 
-    function docIteratorFn(testSuiteNode) {
-        let testsAttr = testSuiteNode.getAttribute("tests");
-        if (testsAttr) {
-            tests += parseInt(testsAttr, 10);
-        }
-
-        let failuresAttr = testSuiteNode.getAttribute("failures");
-        if (failuresAttr) {
-            failures += parseInt(failuresAttr, 10);
-        }
-
-        let errorsAttr = testSuiteNode.getAttribute("errors");
-        if (errorsAttr) {
-            errors += parseInt(errorsAttr, 10);
-        }
-        combinedTestSuitesNode.appendChild(testSuiteNode);
-    }
-
-    srcStrings.forEach(function (srcString) {
-        let doc = new DOMParser().parseFromString(srcString, "text/xml");
-        let nodes = doc.getElementsByTagName("testsuite"),
+    srcStrings.forEach((srcString) => {
+        const doc = new DOMParser().parseFromString(srcString, "text/xml");
+        const nodes = doc.getElementsByTagName("testsuite"),
             nodeCount = nodes.length;
-
         for (let i = 0; i < nodeCount; ++i) {
-            docIteratorFn(nodes[i]);
+            const testSuiteNode = nodes[i];
+            for (const attr in attrs) {
+                attrs[attr] += Number(testSuiteNode.getAttribute(attr));
+            }
+            combinedTestSuitesNode.appendChild(testSuiteNode);
         }
     });
 
-    combinedTestSuitesNode.setAttribute("tests", tests);
-    combinedTestSuitesNode.setAttribute("failures", failures);
-    combinedTestSuitesNode.setAttribute("errors", errors);
+    for (const attr in attrs) {
+        combinedTestSuitesNode.setAttribute(attr, attrs[attr]);
+    }
 
     let xmlString = new XMLSerializer().serializeToString(
         combinedTestSuitesNode
