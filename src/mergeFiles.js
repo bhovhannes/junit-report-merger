19b79c7ec595563f918c7ea2a4febb3865b56a2b
Hovhannes Babayan <bhovhannes@gmail.com>
2021-02-26 23:53:05 +0400

    feat: add TypeScript typings

--- src/mergeFiles.js
+++ src/mergeFiles.js
@@ -1,50 +1,64 @@
-const fs = require("fs");
-const fastGlob = require("fast-glob");
-const { normalizeArgs } = require("./helpers.js");
-const { mergeStreams } = require("./mergeStreams.js");
+const fs = require('fs')
+const fastGlob = require('fast-glob')
+const { normalizeArgs } = require('./helpers.js')
+const { mergeStreams } = require('./mergeStreams.js')
 
 /**
- * @typedef {
- *  {
- *    onFileMatched?: function(matchInfo: {filePath: string}): void
- *  }
- * } MergeFilesOptions
-
-/**
- * Reads multiple files, merges their contents and write into the given file.
+ * @typedef {Object} MatchInfo  Describes a single file match which will be processed
+ * @property {string} filePath   Path to the file
+ *
+ * @callback MergeFilesCallback
+ * @param {MatchInfo} matchInfo
+ * @returns {void}
+ *
+ * @typedef {Object} MergeFilesOptions
+ * @property {MergeFilesCallback} [onFileMatched]  A callback function which will be called for the each match
+ *
+ * @callback TMergeFilesCompletionCallback
+ * @param {Error | null} [err]  Error if any
+ * @return {void}
+ *
+ *
+ * @callback MergeFilesCallbackStyle Reads multiple files, merges their contents and write into the given file.
+ * @param {String} destFilePath   Where the output should be stored. Denotes a path to file. If file already exists, it will be overwritten.
+ * @param {String[]} srcFilePathsOrGlobPatterns   Paths to the files which should be merged or glob patterns to find them.
+ * @param {MergeFilesOptions} options   Merge options.
+ * @param {TMergeFilesCompletionCallback} cb   Callback function which will be called at completion. Will receive error as first argument if any.
+ * @return {void}
+ *
+ * @callback MergeFilesPromiseStyle Reads multiple files, merges their contents and write into the given file.
  * @param {String} destFilePath   Where the output should be stored. Denotes a path to file. If file already exists, it will be overwritten.
  * @param {String[]} srcFilePathsOrGlobPatterns   Paths to the files which should be merged or glob patterns to find them.
  * @param {MergeFilesOptions} [options]   Merge options. Currently unused.
- * @param {Function} [cb]   Callback function which will be called at completion. Will receive error as first argument if any.
+ * @return {Promise<void>}
+ *
+ * @typedef {MergeFilesCallbackStyle & MergeFilesPromiseStyle} MergeFilesFn
+ *
+ * @type {MergeFilesFn}
  */
-function mergeFiles(destFilePath, srcFilePathsOrGlobPatterns, options, cb) {
-    const { callback, normalizedOptions, returnValue } = normalizeArgs(
-        options,
-        cb
-    );
+module.exports.mergeFiles = function (destFilePath, srcFilePathsOrGlobPatterns, options, cb) {
+  const { callback, normalizedOptions, returnValue } = normalizeArgs(options, cb)
 
-    fastGlob(srcFilePathsOrGlobPatterns, { dot: true }).then((srcFilePaths) => {
-        const srcStreams = srcFilePaths.map(function (srcFilePath) {
-            if (normalizedOptions.onFileMatched) {
-                normalizedOptions.onFileMatched({
-                    filePath: srcFilePath,
-                });
-            }
-            return fs.createReadStream(srcFilePath, {
-                flags: "r",
-                encoding: "utf8",
-                autoClose: true,
-            });
-        });
-        const destStream = fs.createWriteStream(destFilePath, {
-            flags: "w",
-            defaultEncoding: "utf8",
-            autoClose: true,
-        });
-        mergeStreams(destStream, srcStreams, {}, callback);
-    }, callback);
+  fastGlob(srcFilePathsOrGlobPatterns, { dot: true }).then((srcFilePaths) => {
+    const srcStreams = srcFilePaths.map(function (srcFilePath) {
+      if (normalizedOptions.onFileMatched) {
+        normalizedOptions.onFileMatched({
+          filePath: srcFilePath
+        })
+      }
+      return fs.createReadStream(srcFilePath, {
+        flags: 'r',
+        encoding: 'utf8',
+        autoClose: true
+      })
+    })
+    const destStream = fs.createWriteStream(destFilePath, {
+      flags: 'w',
+      defaultEncoding: 'utf8',
+      autoClose: true
+    })
+    mergeStreams(destStream, srcStreams, {}, callback)
+  }, callback)
 
-    return returnValue;
+  return returnValue
 }
-
-module.exports = { mergeFiles };
--- src/mergeToString.js
+++ src/mergeToString.js
@@ -1,50 +1,52 @@
-const { create } = require("xmlbuilder2");
+const { create } = require('xmlbuilder2')
+
+/**
+ * @typedef {{}} MergeStringsOptions
+ */
 
 /**
  * Merges contents of given XML strings and returns resulting XML string.
  * @param {String[]} srcStrings   Array of strings to merge together.
- * @param {Object} [options]   Merge options. Currently unused.
+ * @param {MergeStringsOptions} [options]   Merge options. Currently unused.
  * @return {String}
  */
-function mergeToString(srcStrings, options) {
-    const targetDoc = create({
-        testsuites: {},
-    });
+module.exports.mergeToString = function (srcStrings, options) {
+  const targetDoc = create({
+    testsuites: {}
+  })
 
-    const attrs = {
-        failures: 0,
-        errors: 0,
-        tests: 0,
-    };
+  const attrs = {
+    failures: 0,
+    errors: 0,
+    tests: 0
+  }
 
-    srcStrings.forEach((srcString) => {
-        const doc = create(srcString, {});
+  srcStrings.forEach((srcString) => {
+    const doc = create(srcString, {})
 
-        doc.root().each(
-            (xmlBuilder) => {
-                if (xmlBuilder.node.nodeName.toLowerCase() === "testsuite") {
-                    for (const attrNode of xmlBuilder.node.attributes) {
-                        const name = attrNode.name;
-                        if (name in attrs) {
-                            attrs[name] += Number(attrNode.value);
-                        }
-                    }
-                    targetDoc.root().import(xmlBuilder);
-                }
-            },
-            true,
-            true
-        );
-
-        for (const attr in attrs) {
-            targetDoc.root().att(attr, attrs[attr]);
+    doc.root().each(
+      (xmlBuilder) => {
+        if (xmlBuilder.node.nodeName.toLowerCase() === 'testsuite') {
+          for (const attrNode of xmlBuilder.node.attributes) {
+            const name = attrNode.name
+            if (name in attrs) {
+              attrs[name] += Number(attrNode.value)
+            }
+          }
+          targetDoc.root().import(xmlBuilder)
         }
-    });
+      },
+      true,
+      true
+    )
 
-    return targetDoc.toString({
-        prettyPrint: true,
-        noDoubleEncoding: true,
-    });
+    for (const attr in attrs) {
+      targetDoc.root().att(attr, attrs[attr])
+    }
+  })
+
+  return targetDoc.toString({
+    prettyPrint: true,
+    noDoubleEncoding: true
+  })
 }
-
-module.exports = { mergeToString };
